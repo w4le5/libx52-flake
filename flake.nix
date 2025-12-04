6065a5917a27fd9dd1ee15af8140b4f6e58ff4f8
wa'leS <112171624+w4le5@users.noreply.github.com>
2025-12-04 20:05:46 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,57 +1,74 @@
-outputs = { self, nixpkgs, flake-utils }:
-  flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
+{
+  description = "libx52 — Library and daemon for Saitek X52/X52Pro/X55 flight sticks";
 
-      libx52 = pkgs.stdenv.mkDerivation rec {
-        pname = "libx52";
-        version = "unstable-2025-12-05";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-        src = pkgs.fetchFromGitHub {
-          owner = "nirenjan";
-          repo = "libx52";
-          rev = "master";
-          sha256 = "sha256-xVOwNinQZ3CLIRoeiIZ90gn/mVsUskCEam72UfMTkjQ=";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        libx52 = pkgs.stdenv.mkDerivation rec {
+          pname = "libx52";
+          version = "unstable-2025-12-05";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "nirenjan";
+            repo = "libx52";
+            rev = "master";
+            sha256 = "sha256-xVOwNinQZ3CLIRoeiIZ90gn/mVsUskCEam72UfMTkjQ=";
+          };
+
+          nativeBuildInputs = with pkgs; [
+            autoconf automake gettext libtool pkg-config
+          ];
+
+          buildInputs = with pkgs; [
+            hidapi libusb1 libevdev python3
+          ];
+
+          preConfigure = "./autogen.sh";
+
+          configureFlags = [
+            "--localstatedir=${placeholder "out"}/var"
+            "--sysconfdir=${placeholder "out"}/etc"
+            "--with-input-group=input"
+            "--disable-systemd"
+          ];
+
+          postInstall = ''
+            # Убираем установку udev-прав в системные каталоги
+            sed -i '/install-udevrulesDATA/d' Makefile
+
+            # Копируем правила udev в $out
+            mkdir -p $out/etc/udev/rules.d
+            cp libx52/*.rules $out/etc/udev/rules.d/ 2>/dev/null || true
+          '';
+
+          meta = with pkgs.lib; {
+            description = "C library and daemon for X52/X52Pro/X55 HID devices";
+            homepage = "https://github.com/nirenjan/libx52";
+            license = licenses.mit;
+            platforms = platforms.linux;
+          };
+        };
+      in {
+        packages.libx52 = libx52;
+        packages.default = libx52;
+
+        overlay = final: prev: {
+          libx52 = libx52;
         };
 
-        nativeBuildInputs = with pkgs; [ autoconf automake gettext libtool pkg-config ];
-        buildInputs = with pkgs; [ hidapi libusb1 libevdev python3 ];
-
-        preConfigure = "./autogen.sh";
-
-        configureFlags = [
-          "--localstatedir=${placeholder "out"}/var"
-          "--sysconfdir=${placeholder "out"}/etc"
-          "--with-input-group=input"
-          "--disable-systemd"
-        ];
-
-        postInstall = ''
-          sed -i '/install-udevrulesDATA/d' Makefile
-          mkdir -p $out/etc/udev/rules.d
-          cp libx52/*.rules $out/etc/udev/rules.d/ 2>/dev/null || true
-        '';
-
-        meta = with pkgs.lib; {
-          description = "C library and daemon for X52/X52Pro/X55 HID devices";
-          homepage = "https://github.com/nirenjan/libx52";
-          license = licenses.mit;
-          platforms = platforms.linux;
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            autoconf automake gettext libtool pkg-config
+            hidapi hidapi.dev libusb1 libusb1.dev libevdev libevdev.dev
+            python3 git
+          ];
         };
-      };
-    in {
-      packages.libx52 = libx52;
-      packages.default = libx52;
-
-      overlay = final: prev: {
-        libx52 = libx52;
-      };
-
-      devShells.default = pkgs.mkShell {
-        packages = with pkgs; [
-          autoconf automake gettext libtool pkg-config
-          hidapi hidapi.dev libusb1 libusb1.dev libevdev libevdev.dev
-          python3 git
-        ];
-      };
-    });
+      });
+}
