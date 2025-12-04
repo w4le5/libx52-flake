a07c5030e42f53f613448e0f5a36285bb400b74c
wa'leS <112171624+w4le5@users.noreply.github.com>
2025-12-04 20:02:57 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,36 +1,57 @@
-libx52 = pkgs.stdenv.mkDerivation rec {
-  pname = "libx52";
-  version = "unstable-2025-12-05";
+outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
 
-  src = pkgs.fetchFromGitHub {
-    owner = "nirenjan";
-    repo = "libx52";
-    rev = "master";
-    sha256 = "sha256-xVOwNinQZ3CLIRoeiIZ90gn/mVsUskCEam72UfMTkjQ=";
-  };
+      libx52 = pkgs.stdenv.mkDerivation rec {
+        pname = "libx52";
+        version = "unstable-2025-12-05";
 
-  nativeBuildInputs = with pkgs; [ autoconf automake gettext libtool pkg-config ];
-  buildInputs = with pkgs; [ hidapi libusb1 libevdev python3 ];
+        src = pkgs.fetchFromGitHub {
+          owner = "nirenjan";
+          repo = "libx52";
+          rev = "master";
+          sha256 = "sha256-xVOwNinQZ3CLIRoeiIZ90gn/mVsUskCEam72UfMTkjQ=";
+        };
 
-  preConfigure = "./autogen.sh";
+        nativeBuildInputs = with pkgs; [ autoconf automake gettext libtool pkg-config ];
+        buildInputs = with pkgs; [ hidapi libusb1 libevdev python3 ];
 
-  configureFlags = [
-    "--localstatedir=${placeholder "out"}/var"
-    "--sysconfdir=${placeholder "out"}/etc"
-    "--with-input-group=input"
-    "--disable-systemd"
-  ];
+        preConfigure = "./autogen.sh";
 
-  postInstall = ''
-    sed -i '/install-udevrulesDATA/d' Makefile
-    mkdir -p $out/etc/udev/rules.d
-    cp libx52/*.rules $out/etc/udev/rules.d/ 2>/dev/null || true
-  '';
+        configureFlags = [
+          "--localstatedir=${placeholder "out"}/var"
+          "--sysconfdir=${placeholder "out"}/etc"
+          "--with-input-group=input"
+          "--disable-systemd"
+        ];
 
-  meta = with pkgs.lib; {
-    description = "C library and daemon for X52/X52Pro/X55 HID devices";
-    homepage = "https://github.com/nirenjan/libx52";
-    license = licenses.mit;
-    platforms = platforms.linux;
-  };
-};
+        postInstall = ''
+          sed -i '/install-udevrulesDATA/d' Makefile
+          mkdir -p $out/etc/udev/rules.d
+          cp libx52/*.rules $out/etc/udev/rules.d/ 2>/dev/null || true
+        '';
+
+        meta = with pkgs.lib; {
+          description = "C library and daemon for X52/X52Pro/X55 HID devices";
+          homepage = "https://github.com/nirenjan/libx52";
+          license = licenses.mit;
+          platforms = platforms.linux;
+        };
+      };
+    in {
+      packages.libx52 = libx52;
+      packages.default = libx52;
+
+      overlay = final: prev: {
+        libx52 = libx52;
+      };
+
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          autoconf automake gettext libtool pkg-config
+          hidapi hidapi.dev libusb1 libusb1.dev libevdev libevdev.dev
+          python3 git
+        ];
+      };
+    });
